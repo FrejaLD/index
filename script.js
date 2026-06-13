4880900a02c44acf6779f0186626523e072baab5
FrejaLD <itsfreja@hotmail.com>
2026-06-14 00:45:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-var btn = $('#button');
+const backToTopButton = document.getElementById("backToTopBtn");
 
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
-});
+window.onscroll = function() {scrollFunction()};
 
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
+function scrollFunction() {
+    // Show button if scrolled down 300px
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        backToTopButton.classList.add("show");
+    } else {
+        backToTopButton.classList.remove("show");
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+backToTopButton.addEventListener("click", function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
