d074e03b1f160f22d84d4107fc854c7f67e0249a
FrejaLD <itsfreja@hotmail.com>
2026-06-14 00:19:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-const backToTopButton = document.getElementById("backToTopBtn");
+var btn = $('#button');
 
-window.addEventListener("scroll", () => {
-    // Show button if scrolled down more than 300px
-    if (window.scrollY > 300) {
-        backToTopButton.classList.add("show");
-    } else {
-        backToTopButton.classList.remove("show");
-    }
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
 });
 
-backToTopButton.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
 });
