18f1a7c4dedba40ed4b16f78e2840781c6884bd5
FrejaLD <itsfreja@hotmail.com>
2026-06-18 19:54:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ backToTopButton.addEventListener("click", () => {
     behavior: "smooth"
   });
 });
+
+
+$(document).ready(function() {
+/* show lightbox when clicking a thumbnail */
+    $('a.thumb').click(function(event){
+    	event.preventDefault();
+    	var content = $('.modal-body');
+    	content.empty();
+      	var title = $(this).attr("title");
+      	$('.modal-title').html(title);      	
+      	content.html($(this).html());
+      	$(".modal-profile").modal({show:true});
+    });
+
+  });
